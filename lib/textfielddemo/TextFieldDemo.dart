9f9d7a61a69225f73610896939d01bcbb7c70876
yaoxiawen <xiawen.yxw@alibaba-inc.com>
2019-11-01 17:14:20 +0800

    添加TextFieldDemo

--- lib/textfielddemo/TextFieldDemo.dart
+++ lib/textfielddemo/TextFieldDemo.dart
@@ -8,16 +8,24 @@ class TextFieldDemo extends StatefulWidget {
 }
 
 class TextFieldState extends State<TextFieldDemo> {
-  String _errorText='Here will have a Toast';
+  String _errorText = 'Here will have a Toast';
+  TextEditingController _selectionController = TextEditingController();
+  FocusNode focusNode1 = new FocusNode();
+  FocusNode focusNode2 = new FocusNode();
+  FocusScopeNode focusScopeNode;
 
   @override
   Widget build(BuildContext context) {
+    _selectionController.text = "hello world!";
+    _selectionController.selection = TextSelection(
+        baseOffset: 2, extentOffset: _selectionController.text.length);
     return Scaffold(
       appBar: AppBar(
         title: Text("YXW TextField Demo"),
       ),
       body: Container(
-          margin: EdgeInsets.only(left: 12),
+        margin: EdgeInsets.only(left: 12),
+        child: SingleChildScrollView(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.start,
@@ -34,9 +42,72 @@ class TextFieldState extends State<TextFieldDemo> {
                 },
                 decoration: InputDecoration(
                     hintText: "This is a hint", errorText: _errorText),
-              )
+              ),
+              TextField(
+                //autofocus: true,
+                decoration: InputDecoration(
+                    labelText: "用户名",
+                    hintText: "用户名或邮箱",
+                    prefixIcon: Icon(Icons.person)),
+                //textInputAction: TextInputAction.search,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                    labelText: "密码",
+                    hintText: "您的登录密码",
+                    prefixIcon: Icon(Icons.lock)),
+                obscureText: true,
+                onChanged: (value) {},
+              ),
+              TextField(
+                controller: _selectionController,
+              ),
+              TextField(
+                autofocus: true,
+                focusNode: focusNode1, //关联focusNode1
+                decoration: InputDecoration(labelText: "input1"),
+              ),
+              TextField(
+                focusNode: focusNode2, //关联focusNode2
+                decoration: InputDecoration(labelText: "input2"),
+              ),
+              RaisedButton(
+                child: Text("移动焦点"),
+                onPressed: () {
+                  //将焦点从第一个TextField移到第二个TextField
+                  // 这是一种写法 FocusScope.of(context).requestFocus(focusNode2);
+                  // 这是第二种写法
+                  if (null == focusScopeNode) {
+                    focusScopeNode = FocusScope.of(context);
+                  }
+                  focusScopeNode.requestFocus(focusNode2);
+                },
+              ),
+              RaisedButton(
+                child: Text("隐藏键盘"),
+                onPressed: () {
+                  // 当所有编辑框都失去焦点时键盘就会收起
+                  focusNode1.unfocus();
+                  focusNode2.unfocus();
+                },
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  labelText: "请输入用户名",
+                  prefixIcon: Icon(Icons.person),
+                  // 未获得焦点下划线设为灰色
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey),
+                  ),
+                  //获得焦点下划线设为蓝色
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.greenAccent),
+                  ),
+                ),
+              ),
             ],
-          )
+          ),
+        ),
       ),
     );
   }
