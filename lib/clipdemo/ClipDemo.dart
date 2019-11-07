80723c9630799bd22e794e591a9683ab7e98dc84
yaoxiawen <xiawen.yxw@alibaba-inc.com>
2019-11-07 14:17:17 +0800

    添加ClipDemo

--- lib/clipdemo/ClipDemo.dart
+++ lib/clipdemo/ClipDemo.dart
@@ -4,7 +4,10 @@ import 'package:flutter/material.dart';
 //ClipRRect	将子组件剪裁为圆角矩形
 //ClipRect	剪裁子组件到实际占用的矩形大小（溢出部分剪裁）
 class ClipDemo extends StatelessWidget {
-  final Widget img = Image.asset("images/timg.jpeg",width: 120,);
+  final Widget img = Image.asset(
+    "images/timg.jpeg",
+    width: 120,
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -20,7 +23,8 @@ class ClipDemo extends StatelessWidget {
             children: <Widget>[
               img, //不剪裁
               ClipOval(child: img), //剪裁为圆形
-              ClipRRect( //剪裁为圆角矩形
+              ClipRRect(
+                //剪裁为圆角矩形
                 borderRadius: BorderRadius.circular(5.0),
                 child: img,
               ),
@@ -29,29 +33,59 @@ class ClipDemo extends StatelessWidget {
                 children: <Widget>[
                   Align(
                     alignment: Alignment.topLeft,
-                    widthFactor: .5,//宽度设为原来宽度一半，另一半会溢出
+                    widthFactor: .5, //宽度设为原来宽度一半，另一半会溢出
                     child: img,
                   ),
-                  Text("你好世界", style: TextStyle(color: Colors.green),)
+                  Text(
+                    "你好世界",
+                    style: TextStyle(color: Colors.green),
+                  )
                 ],
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  ClipRect(//将溢出部分剪裁
+                  ClipRect(
+                    //将溢出部分剪裁
                     child: Align(
                       alignment: Alignment.topLeft,
-                      widthFactor: .5,//宽度设为原来宽度一半
+                      widthFactor: .5, //宽度设为原来宽度一半
                       child: img,
                     ),
                   ),
-                  Text("你好世界",style: TextStyle(color: Colors.green))
+                  Text("你好世界", style: TextStyle(color: Colors.green))
                 ],
               ),
+              //剪裁子组件的特定区域，使用CustomClipper。
+              //剪裁成功了，但是图片所占用的空间大小仍然是60×60（红色区域），
+              //这是因为剪裁是在layout完成后的绘制阶段进行的，所以不会影响组件的大小，
+              //这和Transform原理是相似的。
+              DecoratedBox(
+                decoration: BoxDecoration(color: Colors.red),
+                child: ClipRect(
+                  clipper: MyClipper(),
+                  child: img,
+                ),
+              )
             ],
           ),
         ),
       ),
     );
   }
+}
+
+class MyClipper extends CustomClipper<Rect> {
+  @override
+  Rect getClip(Size size) => Rect.fromLTWH(10.0, 15.0, 40.0, 30.0);
+
+  /*
+  shouldReclip() 接口决定是否重新剪裁。
+  如果在应用中，剪裁区域始终不会发生变化时应该返回false，
+  这样就不会触发重新剪裁，避免不必要的性能开销。
+  如果剪裁区域会发生变化（比如在对剪裁区域执行一个动画），
+  那么变化后应该返回true来重新执行剪裁。
+   */
+  @override
+  bool shouldReclip(CustomClipper<Rect> oldClipper) => false;
 }
