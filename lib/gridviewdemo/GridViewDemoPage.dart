1fee19577a16aa15ecb5659f46d4fb4bc54b33c9
yaoxiawen <xiawen.yxw@alibaba-inc.com>
2019-11-11 20:19:21 +0800

    优化

--- lib/gridviewdemo/GridViewDemoPage.dart
+++ lib/gridviewdemo/GridViewDemoPage.dart
@@ -7,46 +7,52 @@ class GridViewDemoPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("YXW GridView Demo"),
-          backgroundColor: Colors.blue,
+      appBar: AppBar(
+        title: Text("YXW GridView Demo"),
+        backgroundColor: Colors.blue,
+      ),
+      body: Container(
+        margin: EdgeInsets.only(left: 12),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            RaisedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => GridViewDemo(title: "横轴固定数量"),
+                  ),
+                );
+              },
+              child: Text("横轴固定数量"),
+            ),
+            RaisedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => GridViewDemo2(title: "横轴子元素为固定最大长度"),
+                  ),
+                );
+              },
+              child: Text("横轴子元素为固定最大长度"),
+            ),
+            RaisedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) =>
+                        GridViewDemo3(title: "GridView.builder"),
+                  ),
+                );
+              },
+              child: Text("GridView.builder"),
+            ),
+          ],
         ),
-        body: Container(
-          margin: EdgeInsets.only(left: 12),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              RaisedButton(
-                onPressed: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => GridViewDemo(title: "横轴固定数量")));
-                },
-                child: Text("横轴固定数量"),
-              ),
-              RaisedButton(
-                onPressed: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => GridViewDemo2(title: "横轴子元素为固"
-                              "定最大长度")));
-                },
-                child: Text("横轴子元素为固定最大长度"),
-              ),
-              RaisedButton(
-                onPressed: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => GridViewDemo3(title: "GridView.builder")));
-                },
-                child: Text("GridView.builder"),
-              ),
-            ],
-          ),
-        ),
+      ),
     );
   }
 }
